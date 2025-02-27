b8718fbfccc3f5930649087a1e8fef78f364859d
Akshay <acharyaakshay445@gmail.com>
2025-02-27 13:24:44 +0530

    adding practice folder

---
+++ practice/variables.tf
@@ -0,0 +1,14 @@
+variable "ami_id" {
+    description = "value for ami id"
+  
+}
+
+variable "instance_type" {
+    description = " value for instance type"
+  
+}
+
+variable "key_name" {
+    description = "key pair"
+  
+}
